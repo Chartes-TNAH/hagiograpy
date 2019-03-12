76165da182851139f66906c5fb17a5ed456d0883
Corentin <corentin.faye@laposte.net>
2019-03-12 14:50:51 +0100

    Mise en place de la base du site avec la base de donnée reliée au site.

---
+++ Hagiograpy/Site/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+chemin_actuel = os.path.dirname(os.path.abspath(__file__))
+templates = os.path.join(chemin_actuel, "templates")
+statics = os.path.join(chemin_actuel, "static")
+
+app = Flask(
+    "Application",
+    template_folder=templates,
+    static_folder=statics
+)
+
+# On configure la base de données
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./db_HagiograPY'
+# On initie l'extension
+db = SQLAlchemy(app)
+
+from .routes import oeuvre, accueil
+
+
+
