6ea0043c360ea8b9c6039fbbdb06c2b7584e488e
intaglio <ojacquot@netcourrier.com>
2019-03-17 18:52:40 +0100

    Rajout des routes pour iiif

--- Hagiograpy/Site/routes.py
+++ Hagiograpy/Site/routes.py
@@ -1,8 +1,28 @@
 from flask import render_template
 
-
 from .app import app
 from .modeles.donnees import Oeuvre, Saint
+
+
+# IIIF Web Service
+@app.route('/<identifier>/info.json')
+def info(identifier):
+    return jsonify(web.info(request.url_root, identifier))
+
+
+@app.route('/<identifier>/<region>/<size>/<rotation>/<quality>.<fmt>')
+def iiif(**kwargs):
+    params = web.Parse.params(**kwargs)
+    path = resolve(params.get('identifier'))
+    with IIIF.render(path, **params) as tile:
+        return send_file(tile, mimetype=tile.mime)
+
+
+def resolve(identifier):
+    """Résolveur d'identifiant iiif vers le chemin de la ressource sur le disque.
+    Cette méthode est spécifique à l'architecture de ce serveur.
+    """
+    return os.path.join(chemin_actuel, 'images', '%s.jpg' % identifier)
 
 
 @app.route("/")
@@ -22,8 +42,8 @@ def oeuvre(vie_id):
     unique_vie=Oeuvre.query.filter(Oeuvre.IdOeuvre==vie_id).first()
     #On fait la requête sur la table Saint mais on l'a filtre en récupérant dans la base à travers la relation oeuvres
     # n'importe qu'elle valeur dont Oeuvre.idOeuvre correspond à la valeur d'entrée
-    saint_vie1 = Saint.query.filter(Saint.oeuvres.any(Oeuvre.IdOeuvre == vie_id)).first()
+    saint_vie1=Saint.query.filter(Saint.oeuvres.any(Oeuvre.IdOeuvre==vie_id)).first()
     saint_vie2=Saint.query.filter(Saint.oeuvres.any(Oeuvre.IdOeuvre==vie_id)).all()
 
-    return render_template("pages/vie.html", nom="Site", oeuvre=unique_vie, saints=saint_vie2,saint=saint_vie1)
+    return render_template("pages/vie.html", nom="Site", oeuvre=unique_vie, saints=saint_vie2, saint=saint_vie1)
 
