ec532ee79e5c8a9d3c62da9b512e6ba5d57c3298
intaglio <ojacquot@netcourrier.com>
2019-03-17 18:48:39 +0100

    Import de IIIF de iiif2

--- Hagiograpy/Site/app.py
+++ Hagiograpy/Site/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify, send_file
 from flask_sqlalchemy import SQLAlchemy
+from iiif2 import IIIF, web
+
 import os
+import os.path
 
 chemin_actuel = os.path.dirname(os.path.abspath(__file__))
 templates = os.path.join(chemin_actuel, "templates")
@@ -12,12 +15,36 @@ app = Flask(
     static_folder=statics
 )
 
+
 # On configure la base de données
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./db_HagiograPY'
 # On initie l'extension
 db = SQLAlchemy(app)
 
 from .routes import oeuvre, accueil
+
+
+#IIIF Web Service
+@app.route('/<identifier>/info.json')
+def info(identifier):
+      return jsonify(web.info(request.url_root, identifier))
+      
+
+@app.route('/<identifier>/<region>/<size>/<rotation>/<quality>.<fmt>')
+def iiif(**kwargs):
+    params = web.Parse.params(**kwargs)
+    path = resolve(params.get('identifier'))
+    with IIIF.render(path, **params) as tile:
+        return send_file(tile, mimetype=tile.mime)
+
+def resolve(identifier):
+    """Résolveur d'identifiant iiif vers le chemin de la ressource sur le disque.
+    Cette méthode est spécifique à l'architecture de ce serveur.
+    """
+    return os.path.join(chemin_actuel, 'images', '%s.jpg' % identifier)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
