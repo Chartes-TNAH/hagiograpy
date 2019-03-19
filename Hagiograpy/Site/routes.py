1026c67a27ad5e193db9975e26c9ae1e3d67431c
intaglio <ojacquot@netcourrier.com>
2019-03-19 23:39:33 +0100

    Suppr de la route @app.route('/<identifier>/<region>/<size>/<rotation>/<quality>.<fmt>')

--- Hagiograpy/Site/routes.py
+++ Hagiograpy/Site/routes.py
@@ -4,27 +4,6 @@ from .app import app
 from .modeles.donnees import Oeuvre, Saint
 
 
-# IIIF Web Service
-@app.route('/<identifier>/info.json')
-def info(identifier):
-    return jsonify(web.info(request.url_root, identifier))
-
-
-@app.route('/<identifier>/<region>/<size>/<rotation>/<quality>.<fmt>')
-def iiif(**kwargs):
-    params = web.Parse.params(**kwargs)
-    path = resolve(params.get('identifier'))
-    with IIIF.render(path, **params) as tile:
-        return send_file(tile, mimetype=tile.mime)
-
-
-def resolve(identifier):
-    """Résolveur d'identifiant iiif vers le chemin de la ressource sur le disque.
-    Cette méthode est spécifique à l'architecture de ce serveur.
-    """
-    return os.path.join(chemin_actuel, 'images', '%s.jpg' % identifier)
-
-
 @app.route("/")
 def accueil():
     oeuvres = Oeuvre.query.all()
@@ -32,18 +11,17 @@ def accueil():
 
 @app.route("/oeuvre/<int:vie_id>")
 def oeuvre(vie_id):
-    """ Création d'une page pour une vie de Saint
+    """Création d'une page pour une vie de Saint
 
     :param vie_id: Id de la vie clé primaire de la table oeuvre dans la base de données
     :type vie_id: texte
     :returns: création de page
-    :rtype: page HTML de la vie souhaité
-    """
+    :rtype: page HTML de la vie souhaitée"""
+
     unique_vie=Oeuvre.query.filter(Oeuvre.IdOeuvre==vie_id).first()
     #On fait la requête sur la table Saint mais on l'a filtre en récupérant dans la base à travers la relation oeuvres
     # n'importe qu'elle valeur dont Oeuvre.idOeuvre correspond à la valeur d'entrée
     saint_vie1=Saint.query.filter(Saint.oeuvres.any(Oeuvre.IdOeuvre==vie_id)).first()
     saint_vie2=Saint.query.filter(Saint.oeuvres.any(Oeuvre.IdOeuvre==vie_id)).all()
-
     return render_template("pages/vie.html", nom="Site", oeuvre=unique_vie, saints=saint_vie2, saint=saint_vie1)
 
