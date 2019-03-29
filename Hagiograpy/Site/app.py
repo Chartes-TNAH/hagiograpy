94e19b7416cb132bc485eb87bd598bdccc2ee787
intaglio <ojacquot@netcourrier.com>
2019-03-29 19:38:12 +0100

    Commit pour heroku

--- Hagiograpy/Site/app.py
+++ Hagiograpy/Site/app.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 import os
 import os.path
 from .constantes import SECRET_KEY
+from flask_migrate import Migrate
 
 
 chemin_actuel = os.path.dirname(os.path.abspath(__file__))
@@ -31,6 +32,8 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 
 from .routes import accueil, oeuvre, inscription, connexion, deconnexion, formulaire, cgu
+
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.run(debug=True)
