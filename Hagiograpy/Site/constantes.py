17f433d7f67577967c1f7397ed1b9502cdde6474
intaglio <ojacquot@netcourrier.com>
2019-03-31 22:40:18 +0200

    config production

--- Hagiograpy/Site/constantes.py
+++ Hagiograpy/Site/constantes.py
@@ -6,6 +6,20 @@ SECRET_KEY = "abfezi456efz"
 if SECRET_KEY == "JE SUIS UN SECRET !":
     warn("Le secret par défaut n'a pas été changé, vous devriez le faire", Warning)
 
+if os.environ.get('DATABASE_URL') is None:
+    class _PRODUCTION:
+        SECRET_KEY = SECRET_KEY
+        # On configure la base de données de production
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///../db_HagiograPY'
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+else:
+    class _PRODUCTION:
+        SECRET_KEY = SECRET_KEY
+        # On configure la base de données de production
+        SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 CONFIG = {
         "production": _PRODUCTION
     }
